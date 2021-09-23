59728b9448841f6c3e8b2c05e46d8e7375b505c4
prinako <nyankonton122@gmail.com>
2021-09-23 04:16:16 -0300

    on the UI

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
-const express = require("express");
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path')
 
 const app = express();
 
-app.get("", (req, res)=>{
-    res.send("hi")
-})
 
-app.listen(3000, () => console.log("on port 3000"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(expressLayout)
+app.set("layout", "./layout/layout")
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.listen(5500, () => console.log("on port 5500"));
